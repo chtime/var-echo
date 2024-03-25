b365145303cd3e12ca34e8e55e3d97da1d7ef3f1
Pascal Stalder <pascal.stalder@zuehlke.com>
2024-03-25 15:29:15 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func envHandler(w http.ResponseWriter, _ *http.Request) {
+	envVar := os.Getenv("LE_ENV_VAR")
+	_, _ = fmt.Fprintf(w, "Value of LE_ENV_VAR: %s", envVar)
+}
+
+func main() {
+	http.HandleFunc("/env", envHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
